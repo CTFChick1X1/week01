c2592f2576774195f0b250e32870ed7291e02af7
CTFChick1X1 <mwangiessy2007@gmail.com>
2025-03-19 23:30:39 +0300

    exercise1 dotnet

---
+++ Exercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string firstname;
+string lastname;
+
+Console.Write("Enter your first name: ");
+firstname = Console.ReadLine() ?? "Unknown";
+
+Console.Write("Enter your last name: ");
+lastname = Console.ReadLine() ?? "Unknown";
+
+Console.WriteLine($"Hello, {firstname} {lastname}!");
